8166e35c0462284cb006777f5469bf332719bd5c
Sylvia Dulaney <SPinegar07@ufl.edu>
2015-11-04 16:27:36 -0600

    created new form that captures personal data and prints it to the console via the formLog function

--- js/main.js
+++ js/main.js
@@ -12,7 +12,22 @@ function clickCounter()	{
 	x = x+1;
 //	document.getElementById("p1").innerHTML = x;
 	var myElement = document.createElement( 'p');
-	myElement.innerHTML = '<p>This is click number ' + x + '</p>';
+	myElement.innerHTML = 'This is click number ' + x;
 	document.getElementById( 'topHeading').appendChild( myElement );
 };
 heading.addEventListener( 'click', clickCounter );
+
+//function stopDefAction(evt) {
+//    evt.preventDefault();
+//}
+    
+//document.getElementById('submitbtn').addEventListener(
+//    'click', stopDefAction, false
+//);
+
+var formLog = document.getElementById('firstname');
+
+document.querySelector('form.mainForm').addEventListener('submit', function (e) {
+	e.preventDefault();
+	console.log(formLog.value);    
+});
